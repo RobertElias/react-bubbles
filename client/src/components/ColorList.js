b2f083cd2b65936097ca01b85ac84222ab52c59f
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-31 09:12:47 -0800

    saveEdit completed

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,6 +21,16 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth().put(`/api/color/${colorToEdit}.id`, colorToEdit)
+      .then( () => {
+        alert('Color has been updated')
+        axiosWithAuth().get('/api/colors')
+        .then(res => updateColors(res.data))
+        .catch(err => console.log(err))
+      })
+      .catch(err => {
+        console.log(err)
+      })
   };
 
   const deleteColor = color => {
